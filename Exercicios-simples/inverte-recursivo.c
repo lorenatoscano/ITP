c3d221487494e14fee9af4b5d4cbe4a1d06891d9
lorenatoscano <luizalorena8@gmail.com>
2019-11-07 14:18:43 -0300

    Adiciona exercicio de recursao

---
+++ Exercicios-simples/inverte-recursivo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void inverte()
+{
+	char letra;
+
+	scanf("%c", &letra);
+
+	if (letra != '\n')
+		inverte();
+		printf("%c", letra);
+}
+
+int main()
+{
+	inverte();
+	printf("\n");
+
+
+	return 0;
+}

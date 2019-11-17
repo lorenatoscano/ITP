19606f4bf22bfde1922d448822efcab2b42819be
lorenatoscano <luizalorena8@gmail.com>
2019-11-17 14:32:16 -0300

    Adiciona opcao de remover bandeira

--- Atividades/campominado2.0/jogo.c
+++ Atividades/campominado2.0/jogo.c
@@ -6,7 +6,7 @@ void jogo(int lin, int col, int bomb)
 	int** campo; //Localizacao das bombas
 	char** exibicao; //O que o jogador ve
 	int** aux; //Marcação dos lugares verificados
-	int i, j, x, y, ok = 1, res;
+	int i, j, x, y, perdeu = 0, ganhou = 0;
 	char action;
 
 	//Aloca as matrizes
@@ -33,15 +33,18 @@ void jogo(int lin, int col, int bomb)
 		system("clear");
 
 		imprime_campo(campo, lin, col);
+		printf("Minas restantes: %d\n\n", bomb);
 		//Imprime o tabuleiro para o usuario
 		imprime_tabuleiro(exibicao, lin, col);
 
 		//Le as coordenadas e a açao
-		printf("Insira as coordenadas e ação que deseja fazer (A para abrir e B para marcar como bomba):\n");
+		printf("Insira as coordenadas e ação que deseja fazer: \n(A - abrir | B - colocar ou retirar bandeira)\n");
 		scanf("%d %d %c", &x, &y, &action);
 		if (x > lin || y > col)
 		{
 			printf("Valores inválidos!\n");
+			getchar();
+			getchar(); 
 		}
 		else
 		{
@@ -53,21 +56,33 @@ void jogo(int lin, int col, int bomb)
 					{
 						exibicao[x - 1][y - 1] = 'x';
 						mostra_mina(campo, exibicao, lin, col);
-						ok = 0;
+						perdeu = 1;
 					}	
-					else 
+					else
+					{
 						checa_mina(x - 1, y - 1, campo, exibicao, aux, lin - 1, col - 1);
+					} 
 					break;
 				case 'B':
-					exibicao[x - 1][y - 1] = 'B';
+					if (exibicao[x - 1][y - 1] == '*')
+					{
+						exibicao[x - 1][y - 1] = 'B';
+						bomb--;
+					}
+					else if (exibicao[x - 1][y - 1] == 'B')
+					{
+						exibicao[x - 1][y - 1] = '*';
+						bomb++;
+					}
 					break;
 				default: 
 					printf("Opção inválida!\n");
 					getchar();
 					getchar(); 
 			}
+			ganhou = verifica_vitoria(exibicao, campo, lin, col);
 		}
 
-	} while(ok);
+	} while(!perdeu && !ganhou);
 }
 

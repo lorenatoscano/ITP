a386cfd5901b5b124de6b154e465f922e7eb4d9d
lorenatoscano <luizalorena8@gmail.com>
2019-11-17 00:03:04 -0300

    Pequenas alterações. Falta adicionar uma condição de vitória

--- Atividades/campominado2.0/jogo.c
+++ Atividades/campominado2.0/jogo.c
@@ -1,47 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-void jogo(int, int, int);
-void prepara_campo(int**, int, int, int);
-void prepara_exibicao(char**, int, int);
-void imprime_tabuleiro(char**, int, int);
-void mostra_mina(int**, char**, int, int);
-void checa_mina(int, int, int**, char**, int, int);
-
-int main()
-{
-	int option;
-
-	srand(time(NULL));
-	system("clear");
-
-	printf("ESCOLHA A DIFICULDADE:\n");
-	printf("1 - Fácil (9x9)\n");
-	printf("2 - Médio (16x16)\n");
-	printf("3 - Difícil (16x30)\n");
-
-	scanf("%d", &option);
-
-	switch(option)
-	{
-		case 1: 
-			jogo(9, 9, 10);
-			break;
-		case 2:
-			jogo(16, 16, 40);
-			break;
-		case 3:
-			jogo(16, 30, 99);
-			break;
-		default: printf("Opção inválida!\n");
-	}
-
-	getchar();
-	getchar();
-
-	return 0;
-}
+#include "jogo.h"
+#include "utils.h"
 
 void jogo(int lin, int col, int bomb)
 {
@@ -68,11 +26,12 @@ void jogo(int lin, int col, int bomb)
 	{
 		system("clear");
 
+		//imprime_campo(campo, lin, col);
 		//Imprime o tabuleiro para o usuario
 		imprime_tabuleiro(exibicao, lin, col);
 
 		//Le as coordenadas e a açao
-		printf("\nInsira as coordenadas e ação que deseja fazer (A para abrir e B para marcar como bomba):\n");
+		printf("Insira as coordenadas e ação que deseja fazer (A para abrir e B para marcar como bomba):\n");
 		scanf("%d %d %c", &x, &y, &action);
 		if (x > lin || y > col)
 		{
@@ -97,99 +56,11 @@ void jogo(int lin, int col, int bomb)
 					break;
 				default: 
 					printf("Opção inválida!\n");
+					getchar();
+					getchar(); 
 			}
 		}
-		getchar();
-		getchar(); 
 
 	} while(ok);
 }
 
-void prepara_campo(int** mat, int lin, int col, int n)
-{
-	//Insere a quantidade de bombas aleatoriamente
-	int i, j;
-
-	while (n > 0)
-	{
-		i = rand() % lin;
-		j = rand() % col;
-
-		while (mat[i][j])
-		{
-			i = rand() % lin;
-			j = rand() % col;
-		}
-
-		mat[i][j] = 1;
-		n--;
-	}
-}
-
-void prepara_exibicao(char** mat, int lin, int col)
-{
-	// Preenche a matriz com asteriscos
-	int i, j;
-  
-    for (i = 0; i < lin; ++i)
-		for (j = 0; j < col; ++j)
-			mat[i][j] = '*';
-}
-
-void imprime_tabuleiro(char** mat, int lin, int col)
-{
-	int i, j;
-
-	//Imprime a primeira linha com os numeros
-	printf("  ");
-	for(i = 1; i <= lin; i++)
-	{
-		if (i < 10) printf(" 0%d", i);
-		else printf(" %d", i);
-	} 
-	printf("\n");
-
-	//Imprime a coluna lateral e o resto da matriz
-	for (i = 0; i < lin; ++i)
-	{
-		if (i < 9) printf("0%d  ", i+1);
-		else printf("%d  ", i+1);
-
-		for (j = 0; j < col; ++j)
-			printf("%c  ", mat[i][j]);
-		printf("\n");
-	}
-
-}
-
-void checa_mina(int x, int y, int** campo, char** exibicao, int lin, int col)
-{
-	int i, j, qtd = 0;
-
-	// //Condicao de parada: há bombas ao redor
-	// if(qtd > 0) 
-	// 	exibicao[x][y] = qtd + '0';
-	// else
-	// {
-		
-	// }
-
-	// printf("Checando minas...\n");
-
-}
-
-void mostra_mina(int** campo, char** exibicao, int lin, int col)
-{
-	int i, j;
-
-	system("clear");
-	imprime_tabuleiro(exibicao, lin, col);
-
-	printf("BOOOM! Você perdeu!\n\n");
-
-	
-	getchar();
-	getchar();
-
-	//Imprime a localizacao de todas as bombas
-}
